c7d593347392f9b370dea496c0003ecafe756b84
Junnnhhh <106327540+Junnnhhh@users.noreply.github.com>
2022-06-09 16:12:29 +0900

    Create leetcode26.java

---
+++ leetcode26.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int cmp = nums[0];
+        int idx = 1;
+        
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i-1]!=nums[i]) {
+                nums[idx++] = nums[i];
+            }
+        }
+        
+        
+        return idx;
+    }
+}
