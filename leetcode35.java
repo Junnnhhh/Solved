fe785125ca130019103a25f7f7ff19436f039ddb
Junnnhhh <106327540+Junnnhhh@users.noreply.github.com>
2022-06-10 16:42:08 +0900

    Create leetcode35.java

---
+++ leetcode35.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums[0] > target) return 0;
+        
+        for(int i=0; i<nums.length-1; i++) {
+            if(nums[i]==target) {
+                return i;
+            } else if(nums[i] < target && nums[i+1] > target) {
+                return i+1;
+            }
+        }
+        
+        return nums[nums.length-1] == target ? nums.length-1 : nums.length;
+    }
+}
